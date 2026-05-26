5686cbdb48b01c94162cf473f928285ef3d82756
Joudktb <joud.khatib2@stud.htw-dresden.de>
2026-05-26 20:30:13 +0200

    Create app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import dashboardRouter from './routes/dashboard.ts';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.json({
+    status: 'OK',
+    message: 'SubscriptionGuard Backend running',
+  });
+});
+
+app.use('/api', dashboardRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
